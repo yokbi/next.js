9d96851316b7c6becd20d1d99641f76ef7a08fe5
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-11-05 12:23:30 +0100

    Add config

--- packages/next/src/server/config-schema.ts
+++ packages/next/src/server/config-schema.ts
@@ -298,6 +298,7 @@ export const experimentalSchema = {
   turbopackFileSystemCacheForBuild: z.boolean().optional(),
   turbopackSourceMaps: z.boolean().optional(),
   turbopackTreeShaking: z.boolean().optional(),
+  turbopackRemoveUnusedImports: z.boolean().optional(),
   turbopackRemoveUnusedExports: z.boolean().optional(),
   turbopackScopeHoisting: z.boolean().optional(),
   turbopackImportTypeBytes: z.boolean().optional(),
